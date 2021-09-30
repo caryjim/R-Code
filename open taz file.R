5427a639896c19b10aa491fc4183371387ade772
Cary K. Jim <21142018+caryjim@users.noreply.github.com>
2021-09-30 18:49:43 -0500

    open and extract tar files

---
+++ open taz file.R
@@ -0,0 +1,3 @@
+setwd("D:/Documents/R/Digital Divide")
+untar(tarfile="D:/Downloads/2020Q1Q2.tar", list=T)#Check the list within the tar file 
+untar(tarfile="D:/Downloads/2020Q1Q2.tar", exdir = "D:/Documents/R/Digital Divide/mobile")
