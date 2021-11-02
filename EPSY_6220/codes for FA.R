efbaf0fc66fe3ba46c2513ccac5bb8b9d132c0f7
Cary K. Jim <21142018+caryjim@users.noreply.github.com>
2021-11-01 21:31:39 -0500

    Delete codes for FA.R

--- EPSY_6220/codes for FA.R
+++
@@ -1,4 +0,0 @@
-setwd("~/OneDrive - UNT System/1_EPSY 6220/R/Classwork")
-library(readr)
-test4 <- read_csv("~/OneDrive - UNT System/1_EPSY 6220/R/10.29.2019 Factor Analysis in R/test4.csv")
-library(psych)
