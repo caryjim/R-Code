9296c6692f37498252c09059882667720f9d26e0
Cary K. Jim <21142018+caryjim@users.noreply.github.com>
2021-11-01 21:31:26 -0500

    Update Reliabilty_and_factor_structure_example.R

--- EPSY_6220/Reliabilty_and_factor_structure_example.R
+++ EPSY_6220/Reliabilty_and_factor_structure_example.R
@@ -1,16 +1,8 @@
-#We would like to obtain classical test statistics on a test we administered to 100
-#people. Then we want to examine the factor structure of that test.
-
-#First thing to do is get the data, which is in a CSV file called "test4"
-#Using the "Import Dataset" feature on the right side of your screen, import the dataset from your
-#computer after obtaining it from Blackboard.
+#Script from class exercise
 test4 <- read.csv("~/Documents/Dropbox/NASCAR Drive/Courses/EPSY 6220/test4.csv")
 View(test4)
 
-#If you don't see the "psych" package listed when you select the "Packages" tab in the lower
-#right part of your screen, you need to select "Install" and install the package. Once it is
-#in the list of available packages, select the check box so that it is loaded into R.
-library("psych", lib.loc="/Library/Frameworks/R.framework/Versions/3.2/Resources/library")
+library(psych)
 
 #now that we have the psych package available, we can ask it to compute the reliability
 #of the data in test4.csv
